c4676b0d22fdf9427b8866d3da7875aaf120b65f
Linah02 <120464421+Linah02@users.noreply.github.com>
2023-05-05 05:42:24 +0200

    Sprint2
    Sprint2

--- WEB-INF/classes/src/Url.java
+++
@@ -1,9 +0,0 @@
-package etu001982.framework.myAnnotations;
-import java.lang.annotation.*;
-@Documented
-@Target({ElementType.TYPE,ElementType.METHOD})
-@Inherited
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Url {
-    String name () default "";
-}
